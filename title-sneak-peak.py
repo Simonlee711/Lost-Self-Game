1d6c1f01db420315704a30e4d27894097d1b6bd6
Simonlee711 <simonlee711@gmail.com>
2022-11-12 20:33:55 -0800

    new gif

--- title-sneak-peak.py
+++ title-sneak-peak.py
@@ -6,5 +6,4 @@ images = list(image_path.glob('*.png'))
 image_list = []
 for file_name in images:
     image_list.append(imageio.imread(file_name))
-
     imageio.mimwrite('animated_from_images.gif', image_list)
